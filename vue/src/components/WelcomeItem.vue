caaa5a790c7a024e7291803545effefe88cb846a
Søren Thomassen <115066094+st-randers@users.noreply.github.com>
2024-01-15 18:17:08 +0000

    Simplified and cleaned Vite app landing page

--- vue/src/components/WelcomeItem.vue
+++ vue/src/components/WelcomeItem.vue
@@ -1,13 +1,7 @@
 <template>
-  <div class="item">
-    <i>
-    </i>
-    <div class="details">
-      <h3>
-        <slot name="icon"></slot>&nbsp;
-        <slot name="heading"></slot>
-      </h3>
-      <slot></slot>
-    </div>
-  </div>
+    <h3>
+      <slot name="icon"></slot>&nbsp;
+      <slot name="heading"></slot>
+    </h3>
+    <slot></slot>
 </template>
